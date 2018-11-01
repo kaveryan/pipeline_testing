20703f11db95aeeabe94317744caad2d0c816c78
kaver <kaver.yan@outlook.com>
2018-11-01 16:00:59 +0800

    8

--- initjob.groovy
+++ initjob.groovy
@@ -1,8 +1,2 @@
-import hudson.model.*
-import hudson.AbortException
-import hudson.console.HyperlinkNote
-import java.util.concurrent.CancellationException
-
-user = hudson.model.User.get('username')
-prop = user.getProperty(jenkins.security.ApiTokenProperty.class)
-println(prop.getApiToken())
+def aaa = build.buildVariableResolver.resolve("installlist")
+echo $aaa
