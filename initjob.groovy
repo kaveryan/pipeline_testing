6c3903e949fa3b0a39e05e05a0354e11a40f42c4
kaver <kaver.yan@outlook.com>
2018-11-01 16:39:35 +0800

    12

--- initjob.groovy
+++ initjob.groovy
@@ -1,2 +1,4 @@
-def userInput = input ( message : 'Select deployment versión and input deployment code:',
-     parameters: [[$class: 'TextParameterDefinition', defaultValue: '', description: 'Clarive code', name: 'code']] )
+stage("输出安装列表"){
+  def userInput = input ( message : 'Select deployment versión and input deployment code:',
+       parameters: [[$class: 'TextParameterDefinition', defaultValue: '', description: 'Clarive code', name: 'code']] )
+}
