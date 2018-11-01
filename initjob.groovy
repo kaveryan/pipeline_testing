5f2584267642ed6bb3753c7fb70b22e5f566e0a1
kaver <kaver.yan@outlook.com>
2018-11-01 22:30:15 +0800

    18

--- initjob.groovy
+++ initjob.groovy
@@ -3,13 +3,14 @@ pipeline {
     agent any
     parameters {
         text(name:'installlist', defaultValue: 'nginx', description: 'install list')
-        string(name:'installlist', defaultValue: 'nginx', description: 'User name')
-        password(name:'installlist', defaultValue: 'nginx', description: 'password')
+        string(name:'username', defaultValue: 'nginx', description: 'User name')
+        password(name:'password', defaultValue: 'nginx', description: 'password')
     }
     stages{
         stage("Component L"){
             steps{
                   script{
+                      echo "${params.installlist}"
                       installlist = "${params.installlist}"
                       sh 'python createplaybook.py -i $installlist'
                       // user = input ( message : 'Select deployment versión and input deployment code:', parameters: [[$class: 'TextParameterDefinition', defaultValue: '', description: 'Clarive code', name: 'code']] )
