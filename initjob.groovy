7005c845cb3c744267b4984b073f8f61aae283f1
kaver <kaver.yan@outlook.com>
2018-11-01 16:26:04 +0800

    10

--- initjob.groovy
+++ initjob.groovy
@@ -5,7 +5,6 @@ def thr = Thread.currentThread()
 // get current build
 def build = thr?.executable
 
-
 // get parameters
 def parameters = build?.actions.find{ it instanceof ParametersAction }?.parameters
 parameters.each {
@@ -13,12 +12,9 @@ parameters.each {
    println it.dump()
    println "-" * 80
 }
-
-
 // ... or if you want the parameter by name ...
-def hardcoded_param = "FOOBAR"
+def hardcoded_param = "installlist"
 def resolver = build.buildVariableResolver
 def hardcoded_param_value = resolver.resolve(hardcoded_param)
-
 
 println "param ${hardcoded_param} value : ${hardcoded_param_value}"
