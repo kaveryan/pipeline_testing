6c1b125cc8f7bcd39d0b38731615bcd9f1f4b324
kaver <kaver.yan@outlook.com>
2018-11-01 16:48:28 +0800

    13

--- initjob.groovy
+++ initjob.groovy
@@ -1,4 +1,16 @@
-stage("输出安装列表"){
-  def userInput = input ( message : 'Select deployment versión and input deployment code:',
-       parameters: [[$class: 'TextParameterDefinition', defaultValue: '', description: 'Clarive code', name: 'code']] )
+pipeline {
+    stage("Component List"){
+    parameters {
+        string(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?')
+
+        text(name: 'BIOGRAPHY', defaultValue: '', description: 'Enter some information about the person')
+
+        booleanParam(name: 'TOGGLE', defaultValue: true, description: 'Toggle this value')
+
+        choice(name: 'CHOICE', choices: ['One', 'Two', 'Three'], description: 'Pick something')
+
+        password(name: 'PASSWORD', defaultValue: 'SECRET', description: 'Enter a password')
+
+        file(name: "FILE", description: "Choose a file to upload")
+    }
 }
